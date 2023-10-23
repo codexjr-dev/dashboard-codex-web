46fe28a4278a7eb440b8ae302a05dbb81b5c897e
Matheus Victor <matheusvictor.dev@gmail.com>
2023-10-22 22:59:59 -0300

    fix: correct display of users birth dates and project start date

--- src/pages/Member.vue
+++ src/pages/Member.vue
@@ -83,6 +83,7 @@ import AdicionarMembro from '@/components/modals/AdicionarMembro.vue'
 import { ElNotification, ElMessageBox } from 'element-plus'
 import models from '@/constants/models'
 import { cloneDeep } from 'lodash'
+import moment from 'moment';
 
 export default {
    name: 'Member',
@@ -151,7 +152,7 @@ export default {
       },
 
       formatDate(row, column, prop) {
-         return prop ? Utils.formatDate(prop) : '-'
+         return prop ? moment.utc(prop).format('DD/MM/YYYY') : '-';
       },
 
       closeModal() {
