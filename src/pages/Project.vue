5bd1846316e26a3f08d5f1b63961a4ff42607589
ManoMax <gabriel.matos@ccc.ufcg.edu.br>
2023-11-01 18:48:16 -0300

    refactor: store patten and modal use updated, and pugjs removed

--- src/pages/Project.vue
+++ src/pages/Project.vue
@@ -1,117 +1,83 @@
-<template lang="pug">
-div
-   el-card
-      el-table(
-         :data="dados"
-         stripe
-      )
-         el-table-column(
-            prop="name",
-            label="Nome",
-            :width="150"
-         )
-         el-table-column(
-            prop="description",
-            label="Descrição",
-         )
-         el-table-column(
-            prop="team",
-            label="Time",
-            :formatter="formatList"
-            :width="210"
-         )
-         el-table-column(
-            prop="startDate",
-            label="Data de início",
-            :formatter="formatDate"
-            :width="150"
-         )
-         el-table-column(
-            label="Ações"
-            align="right"
-         )
-            template(
-               #default="scope"
-            )
-               div.actions()
-                  div.actions-button(
-                     v-if="isLeadership || onTeam(scope.row)"
-                     @click="handleAddNews(scope.$index, scope.row)"
-                     :style="'background: #A8CDE8'"
-                  )
-                     el-icon
-                        Plus()
-                  div.actions-button(
-                     @click="handleViewNews(scope.$index, scope.row)"
-                     :style="'background: #E8A8CE'"
-                  )
-                     el-icon
-                        List()
-                  div.actions-button(
-                     v-if="isLeadership"
-                     @click="handleEditProject(scope.$index, scope.row)"
-                     :style="'background: #4b53c6'"
-                  )
-                     el-icon
-                        Edit()
-                  div.actions-button(
-                     @click="handleViewProject(scope.$index, scope.row)"
-                     :style="'background: #67c23a'"
-                  )  
-                     el-icon
-                        View()
-                  div.actions-button(
-                     v-if="isLeadership"
-                     @click="handleDeleteProject(scope.$index, scope.row)"
-                     :style="'background: #e07c72'"
-                  )
-                     el-icon
-                        DeleteFilled()
-   el-dialog(
-      center
-      :before-close="handleClose"
-      :title="titleModal"
-      @close="closeModal"
-      v-model="showModal"
-      fullscreen=true
-   )
-      adicionar-projeto(
-         :titleModal='titleModal'
-         :isVisualizar="isVisualizar"
-         :projeto="novoProjeto"
-      )
-      template(
-         #footer
-      )
-         span.dialog-footer
-            el-button(
-               v-if="!isVisualizar"
-               @click="isEditar ? editar() : salvar()"
-               type="primary"
-               color="#4b53c6"
-            ) Salvar
+<template>
+   <div>
+      <el-card>
+         <el-table :data="dados" stripe>
+            <el-table-column prop="name" label="Nome" :width="150"></el-table-column>
+            <el-table-column prop="description" label="Descrição"></el-table-column>
+            <el-table-column prop="team" label="Time" :formatter="formatList" :width="210">
+               <template v-slot="scope">
+                  <div v-for="(member, index) in scope.row.team" :key="index">
+                     <div>{{ member.name }}</div>
+                  </div>
+               </template>
+            </el-table-column>
+            <el-table-column prop="startDate" label="Data de início" :formatter="formatDate"
+               :width="150"></el-table-column>
+            <el-table-column label="Ações" align="right">
+               <template v-slot="scope">
+                  <div class="actions">
+                     <div class="actions-button" v-if="isLeadership || onTeam(scope.row)"
+                        @click="handleAddNews(scope.$index, scope.row)" :style="'background: #A8CDE8'">
+                        <el-icon>
+                           <Plus />
+                        </el-icon>
+                     </div>
+                     <div class="actions-button" @click="handleViewNews(scope.$index, scope.row)"
+                        :style="'background: #E8A8CE'">
+                        <el-icon>
+                           <List />
+                        </el-icon>
+                     </div>
+                     <div class="actions-button" v-if="isLeadership" @click="handleEditProject(scope.$index, scope.row)"
+                        :style="'background: #4b53c6'">
+                        <el-icon>
+                           <Edit />
+                        </el-icon>
+                     </div>
+                     <div class="actions-button" @click="handleViewProject(scope.$index, scope.row)"
+                        :style="'background: #67c23a'">
+                        <el-icon>
+                           <View />
+                        </el-icon>
+                     </div>
+                     <div class="actions-button" v-if="isLeadership" @click="handleDeleteProject(scope.$index, scope.row)"
+                        :style="'background: #e07c72'">
+                        <el-icon>
+                           <DeleteFilled />
+                        </el-icon>
+                     </div>
+                  </div>
+               </template>
+            </el-table-column>
+         </el-table>
+      </el-card>
 
-   el-dialog(
-      center
-      :before-close="handleClose"
-      :title="titleModal"
-      @close="closeModal"
-      v-model="showModalAddNews"
-   )
-      add-news-modal(
-         :titleModal='titleModal'
-         :news="newsToBeCreated"
-      )
-      template(
-         #footer
-      )
-         span.dialog-footer
-            el-button(
-               @click="saveNews()"
-               type="primary"
-               color="#4b53c6"
-            ) Salvar
+      <el-dialog center :before-close="handleClose" :title="titleModal" @close="closeModal" v-model="showAddProjectModal"
+         fullscreen="true">
+         <adicionar-projeto :titleModal="titleModal" :isVisualizar="isVisualizar"
+            :projeto="novoProjeto"></adicionar-projeto>
+         <template v-slot:footer>
+            <span class="dialog-footer">
+               <el-button v-if="!isVisualizar" @click="isEditar ? editar() : salvar()" type="primary" color="#4b53c6">
+                  Salvar
+               </el-button>
+            </span>
+         </template>
+      </el-dialog>
+
+      <el-dialog center :before-close="handleClose" :title="titleModal" @close="closeModal" v-model="showAddNewsModal">
+         <add-news-modal :titleModal="titleModal" :news="newsToBeCreated"></add-news-modal>
+         <template v-slot:footer>
+            <span class="dialog-footer">
+               <el-button @click="saveNews()" type="primary" color="#4b53c6">
+                  Salvar
+               </el-button>
+            </span>
+         </template>
+      </el-dialog>
+   </div>
 </template>
+ 
 
 <script>
 import { mapActions } from 'vuex'
@@ -121,7 +87,6 @@ import AddNewsModal from '@/components/modals/AddNewsModal.vue'
 import { ElNotification, ElMessageBox } from 'element-plus'
 import models from '@/constants/models'
 import { cloneDeep } from 'lodash'
-import moment from 'moment';
 
 export default {
    name: 'Project',
@@ -138,9 +103,10 @@ export default {
          message: 'A coleta de projetos pode levar alguns instantes',
          type: 'warning',
       });
-      this.$store.commit('SHOW_SIDEBAR', true);
+
+      this.configHeader();
+
       this.userInfo = await this.getUserInfo();
-      this.$store.commit('SET_SIDEBAR_OPTION', this.$route.name.toLowerCase())
       const res = await this.findAllProjects()
       this.dados = res.projects
       ElNotification.closeAll();
@@ -164,11 +130,11 @@ export default {
    },
 
    computed: {
-      showModal() {
-         return this.$store.state.header.modal === 'projeto'
+      showAddProjectModal() {
+         return this.$store.state.page.modalContext === 'ADD_PROJECT';
       },
-      showModalAddNews() {
-         return this.$store.state.header.modal === "add_news"
+      showAddNewsModal() {
+         return this.$store.state.page.modalContext === 'ADD_NEWS';
       },
       isLeadership() {
          return ['Presidente', 'Diretor(a)'].includes(localStorage.getItem("@role"))
@@ -184,6 +150,13 @@ export default {
          createNews: 'createNews',
          getUserInfo: 'userInfo'
       }),
+
+      configHeader() {
+         this.$store.commit('SET_PAGE_CONTEXT', 'project');
+         this.$store.commit('SET_HEADER_TITLE', 'Projetos');
+         this.$store.commit('SET_HEADER_BUTTON_VISIBILITY', true);
+         this.$store.commit('SHOW_SIDEBAR', true);
+      },
 
       onTeam(row) {
          return this.getTeamMembersId(row).includes(this.userInfo.sub._id);
@@ -209,11 +182,11 @@ export default {
          this.isEditar = false
          this.novoProjeto = cloneDeep(models.emptyProject)
          await this.getProjetos()
-         this.$store.commit('SET_MODAL', '')
+         this.$store.commit('SET_AND_SHOW_MODAL_CONTEXT', '');
       },
 
       handleClose() {
-         this.$store.commit('SET_MODAL', '')
+         this.$store.commit('SET_AND_SHOW_MODAL_CONTEXT', '');
       },
 
       getTeamMembersId(row) {
@@ -230,7 +203,7 @@ export default {
          row.team = this.getTeamMembersId(row);
          this.novoProjeto = row
          this.titleModal = row.name
-         this.$store.commit('SET_MODAL', 'projeto')
+         this.$store.commit('SET_AND_SHOW_MODAL_CONTEXT', 'ADD_PROJECT');
       },
 
       handleEditProject(index, row) {
@@ -239,7 +212,7 @@ export default {
          row.team = this.getTeamMembersId(row);
          this.novoProjeto = row
          this.titleModal = 'Editar projeto'
-         this.$store.commit('SET_MODAL', 'projeto')
+         this.$store.commit('SET_AND_SHOW_MODAL_CONTEXT', 'ADD_PROJECT');
       },
 
       async salvar() {
@@ -251,7 +224,7 @@ export default {
                message: `Projeto ${res.project.name} foi cadastrado com sucesso`,
                type: 'success',
             })
-            this.$store.commit('SET_MODAL', '')
+            this.$store.commit('SET_AND_SHOW_MODAL_CONTEXT', '');
             await this.getProjetos()
             this.novoProjeto = cloneDeep(models.emptyProject)
          } catch (error) { }
@@ -261,7 +234,7 @@ export default {
          try {
             const res = await this.updateProject({ project: this.novoProjeto, id: this.novoProjeto._id })
             this.isEditar = false
-            this.$store.commit('SET_MODAL', '')
+            this.$store.commit('SET_AND_SHOW_MODAL_CONTEXT', '');
             ElNotification.closeAll()
             ElNotification({
                title: 'Tudo certo!',
@@ -276,7 +249,7 @@ export default {
       handleAddNews(index, row) {
          this.novoProjeto = row;
          this.titleModal = 'Adicionar atualização'
-         this.$store.commit('SET_MODAL', 'add_news')
+         this.$store.commit('SET_AND_SHOW_MODAL_CONTEXT', 'ADD_NEWS')
       },
 
       async saveNews() {
@@ -288,7 +261,7 @@ export default {
                message: `Atualização criada com sucesso!`,
                type: 'success',
             });
-            this.$store.commit('SET_MODAL', '');
+            this.$store.commit('SET_AND_SHOW_MODAL_CONTEXT', '');
             this.novoProjeto = cloneDeep(models.emptyProject);
             this.newsToBeCreated = cloneDeep(models.emptyNews);
          } catch (error) { }
